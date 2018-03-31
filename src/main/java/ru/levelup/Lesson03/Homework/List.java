02ff65f70408f6a6f21e942ae218868b79fdd6b4
Nikolai <inikolaikovalenko@yandex.ru>
2018-03-31 21:39:01 +0300

    init commit

--- src/main/java/ru/levelup/Lesson03/Homework/List.java
+++ src/main/java/ru/levelup/Lesson03/Homework/List.java
@@ -3,13 +3,15 @@ package ru.levelup.Lesson03.Homework;
 public class List<T>  {
 
 
-
+    //Добавляет элемент на вершину стека.
     public void push (T valeo){
-//
+
     }
+    //Удаляет элемент с вершины стека и возвращает его. Если стек пустой, кидает InvalidOperationException.
     public T pop(){
 
     }
+    //Возвращает верхний элемент стека, но не удаляет его. Если стек пустой, кидает InvalidOperationException.
     public T peek() {
 
     }
