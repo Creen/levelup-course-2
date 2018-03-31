a03867e59f6f7fc2b353459d6775378c8b518f05
Nikolai <inikolaikovalenko@yandex.ru>
2018-04-01 00:41:18 +0300

    init commit

--- src/main/java/ru/levelup/Lesson03/Homework/List.java
+++ src/main/java/ru/levelup/Lesson03/Homework/List.java
@@ -1,37 +1,54 @@
 package ru.levelup.Lesson03.Homework;
 
+import java.util.EmptyStackException;
+
 public class List<T>  {
 
-    private ListNode head; // указатель на первый элемент
-    private ListNode tail; // указатель на последний элемент
+    private StackElement head; // указатель на первый элемент
+
+    //singly-linked list
+    public class StackElement <T>{
+        //Данные
+        T value;
+
+        //Указатель на следующий слемент
+        StackElement next;
+
+        public StackElement() {
+
+        }
+        StackElement(T value) {
+            this.value = value;
+        }
+    }
 
     //Добавляет элемент на вершину стека.
     public void push (T valeo){
-
-
+            StackElement stackElement = new StackElement();
+            if(head == null){ //если список пуст
+                head = stackElement; //то указываем ссылку начала на новый элемент
+        }  else {
+                stackElement.next = head; //иначе "старый" последний элемент теперь ссылается на новый
+                head = stackElement; //а в указатель на последний элемент записываем адрес нового элемента
+            }
     }
-    //Удаляет элемент с вершины стека и возвращает его. Если стек пустой, кидает InvalidOperationException.
+    //Удаляет элемент с вершины стека и возвращает его.
     public T pop(){
+        if(head == null){
+            throw new EmptyStackException();
+        } else {
+            T value = (T) head.value;
+            head = head.next;
+            return value;
+        }
 
     }
-    //Возвращает верхний элемент стека, но не удаляет его. Если стек пустой, кидает InvalidOperationException.
+    //Возвращает верхний элемент стека, но не удаляет его.
     public T peek() {
 
     }
 
-    //singly-linked list
-    public class ListNode <T>{
-        //Данные
-        T val;
 
-        //Указатель на следующий слемент
-        ListNode next;
-
-        ListNode(T x) {
-            val = x;
-            next = null;
-        }
-    }
 }
 
 
