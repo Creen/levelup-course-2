dd359125db77b9509e88edbe7470fc6571f03c2a
Nikolai <inikolaikovalenko@yandex.ru>
2018-03-31 21:28:57 +0300

    init commit

--- src/main/java/ru/levelup/Lesson03/Homework/StackLinkedList.java
+++ src/main/java/ru/levelup/Lesson03/Homework/StackLinkedList.java
@@ -1,39 +1,32 @@
 package ru.levelup.Lesson03.Homework;
 
-import java.util.EmptyStackException;
-import java.util.LinkedList;
-
 public class StackLinkedList<T>  {
 
-    private T val;
-    private  int size;
-    private  int count;
-    private LinkedList item = new LinkedList();
 
-    public StackLinkedList(T arg){
-        this.val = arg;
-        size = item.size();
-    }
+
     public void push (T valeo){
-//        if (size == count){
-//            throw new StackOverflowError("Стек переполенен");
-//        } else {
-            item.addLast(valeo);
-//        }
-        count++;
+//
     }
     public T pop(){
-        if (item == null){
-            throw new EmptyStackException();
-        }
-        T result = (T) item.getLast();
-        item.removeLast();
-        return result;
+
     }
     public T peek() {
-        if (item == null){
-            throw new EmptyStackException();
+
+    }
+
+    //singly-linked list
+    public class ListNode <T>{
+        //Данные
+        T val;
+
+        //Указатель на следующий слемент
+        ListNode next;
+
+        ListNode(T x) {
+            val = x;
+            next = null;
         }
-        return (T) item.getLast();
     }
 }
+
+
