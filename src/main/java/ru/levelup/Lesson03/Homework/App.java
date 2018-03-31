f2cd3cfd5fba21681b38bcb4375e6f824105123b
Nikolai <inikolaikovalenko@yandex.ru>
2018-04-01 01:00:49 +0300

    init commit

--- src/main/java/ru/levelup/Lesson03/Homework/App.java
+++ src/main/java/ru/levelup/Lesson03/Homework/App.java
@@ -3,14 +3,13 @@ package ru.levelup.Lesson03.Homework;
 public class App {
 
     public static void main(String[] args) {
-        List stack = new List(5);
-        stack.push(3);
-        stack.pop();
-        stack.push(4);
+        List<String> stack = new List<>();
+        stack.push("JavaLove");
+        stack.push("JavaLove2");
+        stack.push("JavaLove3");
 
 
-
-        System.out.println(stack.peek());
+        System.out.println(stack.pop());
 
     }
 }
