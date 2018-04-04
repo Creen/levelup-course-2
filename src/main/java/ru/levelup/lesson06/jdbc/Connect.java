bd2ea7b120b09e455afc6093e435c6ca168e95ac
Nikolai <inikolaikovalenko@yandex.ru>
2018-04-05 01:07:14 +0300

    Changing of Connect.class in process.

--- src/main/java/ru/levelup/lesson06/jdbc/Connect.java
+++ src/main/java/ru/levelup/lesson06/jdbc/Connect.java
@@ -4,10 +4,12 @@ package ru.levelup.lesson06.jdbc;
 import ru.levelup.lesson06.jdbc.configuration.Configuration;
 import ru.levelup.lesson06.jdbc.configuration.PropertiesJdbcConfiguration;
 import ru.levelup.lesson06.jdbc.dao.MysqlDepartmentDao;
+import ru.levelup.lesson06.jdbc.domain.Department;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
+import java.util.Collection;
 import java.util.Properties;
 
 
@@ -20,12 +22,25 @@ public class Connect {
             throw new RuntimeException(e);
         }
     }
+    static String URL = null;
+    static String USER = null;
+    static String PASS = null;
 
     public static void main(String[] args) {
 
         try {
             Configuration configuration = new PropertiesJdbcConfiguration().load();
-        } catch (IOException e) {
+            URL = "jdbc:mysql://" + configuration.getJdbcHost() + ":" +
+                    configuration.getJdbcPort() + "/" + configuration.getJdbcDatabaseName();
+            USER = configuration.getJdbcUsername();
+            PASS = configuration.getJdbcPassword();
+
+            Connection connection = DriverManager.getConnection(URL, USER, PASS);
+            MysqlDepartmentDao dao = new MysqlDepartmentDao(connection);
+            dao. create(1, "Russia", "Moscow");
+            Collection<Department> departments = dao.findAll();
+
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
 
