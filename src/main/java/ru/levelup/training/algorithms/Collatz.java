34f05faebf04c7ce547cee04fb09d3f606b1c176
Nikolai <inikolaikovalenko@yandex.ru>
2018-04-09 20:20:54 +0300

    pull master

---
+++ src/main/java/ru/levelup/training/algorithms/Collatz.java
@@ -0,0 +1,29 @@
+package ru.levelup.training.algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Collatz {
+
+    public static void main(String[] args) {
+
+//        int steps = 0;
+//
+//        System.out.println("Enter some a number");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        try {
+//            int x = Integer.parseInt(br.readLine());
+//            while (x > 1){
+//                if (n ==)
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println("Try again");
+//        } catch (NumberFormatException){
+//            System.out.println("Try again");
+//        }
+//
+    }
+}
