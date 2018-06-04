4a77a4154eb576c0a45d09cb7ef2ee6acba5e907
Nikolai <inikolaikovalenko@yandex.ru>
2018-06-04 23:10:05 +0300

    Add Сollatz

--- src/main/java/ru/levelup/training/algorithms/Collatz.java
+++ src/main/java/ru/levelup/training/algorithms/Collatz.java
@@ -3,27 +3,28 @@ package ru.levelup.training.algorithms;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Collatz {
 
     public static void main(String[] args) {
 
-//        int steps = 0;
-//
-//        System.out.println("Enter some a number");
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        try {
-//            int x = Integer.parseInt(br.readLine());
-//            while (x > 1){
-//                if (n ==)
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.out.println("Try again");
-//        } catch (NumberFormatException){
-//            System.out.println("Try again");
-//        }
-//
+        int steps = 0;
+
+        System.out.println("Enter a number:");
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int x = n;
+
+        while(n > 1){
+            if(n % 2 == 0)
+                n /= 2;
+            else n = (n * 3) + 1;
+            steps++;
+        }
+
+        System.out.println("It takes \'" + steps + "\' steps to reach \'1\' using the Collatz conjecture on the number \'" + x +"\'.");
+
     }
 }
